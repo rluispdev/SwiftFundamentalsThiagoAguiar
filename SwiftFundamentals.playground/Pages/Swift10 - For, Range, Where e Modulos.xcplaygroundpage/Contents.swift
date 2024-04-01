6da242db5bdde2d2afb09668850a86fc7a70167c
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-01 12:43:22 -0300

    For, Range, Where e Modulos

--- SwiftFundamentals.playground/Pages/Swift10 - For, Range, Where e Modulos.xcplaygroundpage/Contents.swift
+++ SwiftFundamentals.playground/Pages/Swift10 - For, Range, Where e Modulos.xcplaygroundpage/Contents.swift
@@ -1,7 +1,41 @@
 //: [Previous](@previous)
 
 import Foundation
+ 
 
-var greeting = "Hello, playground"
+//MARK: Loop For
 
-//: [Next](@next)
+//Sequenciais( Ranges)
+let range = 0...5 // Inclusive
+let r = 0..<5 //Exclusive
+
+var limit = 5
+let rg = 0..<limit
+
+//For é controlado pelo range.
+
+var sum = 0
+let count = 10
+
+for i in 1...count {
+    sum += i
+}
+print(sum)
+
+// Ignorar o índex i fazemos dessa forma
+for _ in 1...count {
+    print("Neymar")
+}
+
+//Fazer comparacão no for usando where.
+for _ in 1...count where count > 56 {
+    print("Luis Fabiano")
+}
+
+//Módulo, é o resto da divisão.
+
+for i in 0...count where i % 2 == 0 {
+    print("Index is \(i)")
+}
+
+
--- SwiftFundamentals.playground/Pages/Swift9 - Repeat.xcplaygroundpage/Contents.swift
+++ SwiftFundamentals.playground/Pages/Swift9 - Repeat.xcplaygroundpage/Contents.swift
@@ -2,6 +2,28 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+//DRY - Don't reapeat yourself - Não repita a si próprio.
+/*
+ While CONDICAO {
+  BLOCO DE LOOPING
+ }
+ */
 
-//: [Next](@next)
+//Com o While ele checa PRIMEIRO a condição.
+var i = 1
+
+while i < 10 {
+    print(i)
+    i += 1
+}
+
+print("Nova contagem")
+
+//Com Repeat, ele faz primeiro uma instrução depois ele checa a condição
+i = 1
+repeat{
+    print(i)
+    i += 1
+}while(i < 10)
+
+ //Em outras liguagens é chamado de do, while.
