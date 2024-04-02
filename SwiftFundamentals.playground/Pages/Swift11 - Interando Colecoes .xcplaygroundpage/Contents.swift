e3e15df17298527bbd00a5d65b805e78494c6e68
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-02 12:44:11 -0300

    Iterar colecoes

--- SwiftFundamentals.playground/Pages/Swift11 - Interando Colecoes .xcplaygroundpage/Contents.swift
+++ SwiftFundamentals.playground/Pages/Swift11 - Interando Colecoes .xcplaygroundpage/Contents.swift
@@ -1,7 +1,18 @@
-//: [Previous](@previous)
+ import UIKit
 
-import Foundation
+let dollars = [5.1, 5.2, 4.9, 5.32, 5.43, 5.11]
+let days = ["01/02", "02/02", "03/02", "04/02", "05/02", "06/02"]
 
-var greeting = "Hello, playground"
+for  i in 0..<dollars.count where dollars [i] < 5.0{
+    print("dolar \(dollars[i]), dolar \(days[i])")
+}
+ 
+//Imprimir todos elementos
 
-//: [Next](@next)
+for day in days {
+    print(day)
+}
+
+for dolar in dollars {
+    print(dolar)
+}
