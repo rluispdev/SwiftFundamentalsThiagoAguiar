bc27056f30dbdca54764136cde96f66cb01f64a8
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-05 12:36:07 -0300

    Dictionary

--- SwiftFundamentals.playground/Pages/Swift13 - Dictionary.xcplaygroundpage/Contents.swift
+++ SwiftFundamentals.playground/Pages/Swift13 - Dictionary.xcplaygroundpage/Contents.swift
@@ -2,6 +2,46 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+var products: [String: Int] = [:]
 
-//: [Next](@next)
+products["Apple"] = 5
+products["Google"] = 16
+
+print(products)
+
+//Acessar um Dict Inicializado
+var products2: [ Int : String] = [
+    1: "Fone de ouvido",
+    2: "Macbook",
+    3: "Impressora",
+    4: "iPhone"
+]
+
+print(products2[4] ?? "Produto não encontrado!")
+
+//Atualizar um dict
+products2[2] = "Macmini"
+print(products2[2])
+
+//Contar elementos
+print(products2.isEmpty)
+print(products2.count)
+
+//Remover
+products2[2] = nil
+print(products)
+
+//Buscar todas as chaves
+for key in products2.keys{
+    print(key)
+}
+
+//Buscar todos os valores
+for val in products2.values{
+    print(val)
+}
+
+//Buscando a chave e o valor
+for (key, value) in products2 {
+    print("chave: \(key), valor: \(value)")
+}
