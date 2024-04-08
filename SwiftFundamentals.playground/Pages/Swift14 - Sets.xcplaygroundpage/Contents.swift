7db7e29b62c50277c3934394aa63d31c37223ad5
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-08 15:56:44 -0300

    Atualizar

--- SwiftFundamentals.playground/Pages/Swift14 - Sets.xcplaygroundpage/Contents.swift
+++ SwiftFundamentals.playground/Pages/Swift14 - Sets.xcplaygroundpage/Contents.swift
@@ -2,6 +2,30 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+// Coleção parecido com array mas não permite valores repetidos.
+var newSet: Set<Int> = []
+newSet.insert(10)
+newSet.insert(3)
+newSet.insert(10)
 
-//: [Next](@next)
+print(newSet)
+
+//Check
+let response = newSet.insert(4)
+print(response)
+print(newSet)
+
+//Verficar se contem o elemento
+print(newSet.contains(3))
+
+// Remover o elemento
+
+if let removed = newSet.remove(4){
+    print(newSet)
+    print(removed)
+}
+
+//Limpar a lista inteira
+newSet.removeAll()
+print(newSet)
+
