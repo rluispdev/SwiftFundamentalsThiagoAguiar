11c5902ef39761ba466a56b7f33c23c38c4596f5
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-17 12:36:06 -0300

    Criar Ferramenta de monitorameto do user no app.

--- SwiftFundamentals.playground/Pages/Swift20 = 4TypesFunctions.xcplaygroundpage/Contents.swift
+++ SwiftFundamentals.playground/Pages/Swift20 = 4TypesFunctions.xcplaygroundpage/Contents.swift
@@ -2,6 +2,35 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+//FUNÇÃO CLÁSSICA - SIMPLES,    TRADICIONAL
+func getFullName(firstName: String, lastName: String) -> String{
+    return firstName + " " + lastName
+}
+    let fullname = getFullName(firstName: "Tiago", lastName: "Aguiar")
+    print(fullname)
+
+//FUNCÃO COM VALOR PADRÃO
+func getFullNameDefault(firstName: String, lastName: String = "Desconhecido") -> String {
+    return firstName + " " + lastName
+}
+
+let fullnamedefault = getFullNameDefault(firstName: "Rafael")
+print(fullnamedefault)
+
+
+//NÃO DEFINIR O NOME DO ARGUMENTO
+func getFullNameNotArgument(_ firstName: String, _ lastName: String) -> String{
+    return firstName + " " + lastName
+}
+let fullnamenotarguemnt = getFullNameNotArgument("Rafael", "Gonzaga")
+print(fullnamenotarguemnt)
+
+//APELIDO PARA OS PARÂMETRO
+
+func getFullNameApelido(name firstName: String, lastName: String) -> String {
+    return firstName + " " + lastName
+}
+let fullnameapelido = getFullNameApelido(name: "Rafael", lastName: "Luis")
+print(fullnameapelido)
+
--- SwiftFundamentals.playground/Pages/Swift21 - Protocol.xcplaygroundpage/Contents.swift
+++ SwiftFundamentals.playground/Pages/Swift21 - Protocol.xcplaygroundpage/Contents.swift
@@ -2,6 +2,50 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+//Cenário - Cirar um app em iOS e vamos que esse app tenha 03 telas
 
-//: [Next](@next)
+//Ferramenta de monitorameto do user no app.
+class GoogleAnalytics {
+    func monitorarEvento(name: String){
+        //A gente faz a chamada aqui
+        let dataDoEvento = Date()
+        print("\(dataDoEvento): \(name)")
+    }
+}
+
+
+///Sem protocol
+//Tela1
+class Login {
+    func fazerLogin(){
+        print("Fazendo login")
+    }
+}
+//Tela2
+class Home{
+    func carregandoProdutos(){
+        print("Carregando produtos")
+    }
+}
+//Tela3
+class Perfil {
+    func editarPerfil(){
+        print("Editando perfil")
+    }
+}
+
+
+let login = Login()
+login.fazerLogin()
+sleep(2)
+
+let perfil = Home()
+perfil.carregandoProdutos()
+sleep(5)
+
+let editPerfil = Perfil()
+editPerfil.editarPerfil()
+
+print("fim do programa")
+
+
