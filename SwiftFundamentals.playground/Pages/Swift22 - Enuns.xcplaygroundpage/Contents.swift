fed2398aa0f755a4d3725c6e156980f49a55dc84
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-19 12:52:54 -0300

    Enum parte1

---
+++ SwiftFundamentals.playground/Pages/Swift22 - Enuns.xcplaygroundpage/Contents.swift
@@ -0,0 +1,71 @@
+//: [Previous](@previous)
+
+import Foundation
+
+ //Cenario = Criar um enum dias da semana
+
+enum DiaDaSemana: String {
+    
+    case segunda = "Monday"
+    case terca = "Tuesday"
+    case quarta = "Wednesday"
+    case quinta = "Thursday"
+    case sexta = "Friday"
+    case sabado = "Saturday"
+    case domingo = "Sunday"
+
+}
+
+print(DiaDaSemana.sexta.rawValue)
+ 
+//Cenario - E-commerce que vende memoria de computador
+
+enum Memory: Int {
+    case small = 2
+    case medio = 4
+    case big = 8
+}
+
+print(Memory.medio.rawValue)
+
+enum CartaoDeCretido {
+    case master
+    case visa
+    case americanXpress
+    
+    var nome: String{
+        switch self {
+            case.master:
+                return "MASTERCARD"
+            case.visa :
+                return "VISA"
+            case.americanXpress:
+                return "American Xpress"
+           
+        }
+        
+    }
+}
+
+print(CartaoDeCretido.master.nome)
+
+
+
+enum StatusPhone{
+    case ligado
+    case desligado
+    
+    func status() -> String {
+        switch self{
+            case .ligado:
+                return "Ligado"
+            case .desligado:
+                return "Ops! Desligado"
+        }
+    }
+}
+
+let phone : StatusPhone = StatusPhone.desligado
+print(phone.status())
+
+
